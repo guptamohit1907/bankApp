a79c9b4dabd50e0af2723862e11e28496ace8610
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-07-21 03:14:14 +0530

    Initial Project setup

---
+++ bankApp/bankApp/bankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  bankAppApp.swift
+//  bankApp
+//
+//  Created by Mohit Gupta on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct bankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
